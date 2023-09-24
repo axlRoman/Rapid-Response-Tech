45760b6f9f3a480ff66e4f485a25cefe68855f57
Francisco Axel Roman Cardoza <franaxel@live.com.mx>
2023-09-24 09:43:05 -0600

    Deteccion de rostros y conseguir coinsidencia con fotos de referencia para validar la informacion

--- Reconocimiento.py
+++ Reconocimiento.py
@@ -1,32 +1,42 @@
 import cv2
-import face_recognition
+import numpy as np
 
 # Abre la cámara
 cap = cv2.VideoCapture(0)
 
 # Cargamos una imagen de referencia para comparar
-reference_image = face_recognition.load_image_file('axel.png')
-reference_encoding = face_recognitionpyth.face_encodings(reference_image)[0]
+reference_image = cv2.imread('axel.png')
+reference_encoding = cv2.cvtColor(reference_image, cv2.COLOR_BGR2GRAY)
+
+# Crea un objeto detector de caras
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
     # Lee un frame de la cámara
     ret, frame = cap.read()
 
-    # Busca caras en el frame
-    face_locations = face_recognition.face_locations(frame)
-    face_encodings = face_recognition.face_encodings(frame, face_locations)
+    # Convierte el frame a escala de grises
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    # Detecta caras en el frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        # Recorta la región de la cara
+        face = gray[y:y+h, x:x+w]
+
         # Compara la cara detectada con la imagen de referencia
-        matches = face_recognition.compare_faces([reference_encoding], face_encoding)
-        name = "Desconocido"
+        result = cv2.matchTemplate(face, reference_encoding, cv2.TM_CCOEFF_NORMED)
+        threshold = 0.7  # Umbral de similitud
 
-        if True in matches:
+        if np.max(result) > threshold:
             name = "Persona de referencia"
+        else:
+            name = "Desconocido"
 
         # Dibuja un rectángulo alrededor de la cara y muestra el nombre
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
     # Muestra el frame con las caras detectadas
     cv2.imshow('Reconocimiento Facial', frame)
